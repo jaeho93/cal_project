aa5696ed7a1d14fa6f65e14ab431cc14ea9d8ae7
Jeonseonjin <wjstjswls123@naver.com>
2016-05-21 22:32:35 +0900

    주석추가

--- calculation.c
+++ calculation.c
@@ -16,15 +16,17 @@ int main(void){
 	
 	for(i=str_a-1;i>=0;i--){
 		printf("%c",a[i]);}
-	printf("\n");
+	printf("\n");             // 입력받은 배열 a  거꾸로 출력
 	
 	for(t=str_b-1;t>=0;t--){
 		printf("%c",b[t]);}
-	printf("\n");
+	printf("\n");            // 입력받은 배열 b 거꾸로 출력
 
 	for(i=str_a-1, t=str_b-1;i>=0||t>=0;i--,t--){
+		// i,t 재설정, i와 t 둘 다 0이하가  될 때까지 for문 반복
+		
 		if(i>=t){
-			c[i]=a[i]+b[t];
+			c[i]=a[i]+b[t]; // a의 길이가 더 길면 c의길이도 a 길이까지
 			
 			/* if(c[i]>9){
 				c[i+1]=c[i]/10;
@@ -32,7 +34,9 @@ int main(void){
 			
 			if(b[t]==0){
 				printf("%d",c[i]-48);}
-			else printf("%d",c[i]-96);}
+			else printf("%d",c[i]-96);}   
+	// b의 배열이 끝나면 a[i] 값의 아스키코드 값이 나옴 => 0의 아스키코드를 빼줌.
+	// a와 b 모두 값이 0이 아닌 경우에는 알파벳소문자로 나옴=> a의 아스키코드를 빼줌
 		
 		else {
 			c[t]=a[i]+b[t];
