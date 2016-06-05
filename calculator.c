3d3f0319a5fcbbeac1822f8dba52a51017179e08
jaeho93 <jaeho8032@gmail.com>
2016-06-05 17:22:13 +0900

    마이너스 버그 수정 160605

--- calculator.c
+++ calculator.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h>	// 단항 연산자 아직 구현 못함.ㅠㅠ								
 #include <string.h>
 #include <stdlib.h>
 #define N 61
@@ -13,36 +13,50 @@ char in1_s[10] = {};
 char in3_s[10] = {};
 char res_j[51] = {};
 char res_s[10] = {};
+int div_j[51] = {};
+
+char c_lear[N] = "clear";
+char e_nd[N] = "end";
 
 int str_in1, str_in2, str_in3, str_res_s;
-int temp = 0, temp1_d = 0, temp3_d = 0, temp_j = 0, temp_s = 0;
+int temp = 0, temp1_d = 0, temp3_d = 0, res_d = 0, temp_j = 0, temp_s = 0;
 int first = 0, last = 0;
-int k = 0, p = 0, q = 0, daeso = 0;
+int k = 0, p = 0, q = 0, daeso = 0, choice = 0; // choice 아직 안씀
 
-//void choice();
+//void choice(); // 명령 선택 (아직 구현 x)
 void separate(); 			// 정수부 소수부 분리
 void invert1();   			// 계산 전 배열 뒤집기
 void invert2();				// 계산 후 배열 뒤집기
 void compare();				// 앞뒤수 비교
+void compare_div();			// 나누기용 앞뒤수 비교
 void convert1();			// char > int 변환
 void convert2();			// int > char 변환
+void convert3();
 void plus();
 void minus();
+void minus_div();			// 나누기용 마이너스
 void mutiply();
 void divide();
 void remain();
-void clear();
 void end();
 void VAR();
+void initialization();
 
 int main() {
 
 start:
 
-	printf("(input) ");			//계산기 프로그램 시작
+	printf("\n(input) ");			//계산기 프로그램 시작
 	
 	scanf("%s", in1);
 	str_in1 = strlen(in1);
+/*																// strcmp  오류
+	choice = strcmp (in1, c_lear);
+	if (choice == 0) {system("clear"); goto start;}
+	else{
+	choice = strcmp (in1, e_nd);
+	if (choice == 0) exit(1);}
+*/		
 
 	scanf("%s", in2);
 	str_in2 = strlen(in2);
@@ -54,10 +68,10 @@ start:
 
 	switch (in2[0]) 				{		
 	
-	/*case '+' : { plus(); }*/
-	case '-' : { compare(); invert1(); convert1(); minus(); convert2(); invert2(); }
+	case '+' : { invert1(); convert1(); plus(); convert2(); invert2(); break; }
+	case '-' : { invert1(); compare(); convert1(); minus(); convert2(); invert2(); break; }
 	/*case '*' : { multiply(); }*/
-	/*case '/' : { compare(); invert1(); convert1(); divide(); convert2(); invert2();}*/
+	case '/' : { compare(); invert1(); convert1(); divide(); convert2(); invert2(); break; }
 
 	default: ;
 									}
@@ -67,10 +81,12 @@ start:
 	printf("-");
 	printf("%s", res_j);
 	if (p != 0)
-	printf(".%s/n", res_s);
+	printf(".%s\n", res_s);
+
+    initialization();
+
 	goto start;
 
-end:
 	return 0;
 }
 
@@ -140,12 +156,12 @@ void invert2(){			// 계산 후 정수부 배열을 뒤집는 함수
 
 void compare(){				// 빼기, 나누기 전 크기 비교하는 함수
 
-	if (temp3_d > temp1_d)
+	if (strlen(in3_j) > strlen(in1_j))
 	daeso = 1;
-	else if(temp3_d < temp1_d)
+	else if(strlen(in3_j) < strlen(in1_j))
 	{daeso = 0; goto dae;}
 	else{
-	for (int i = 0; i <= 49; i++)
+	for (int i = strlen(in1_j); i >= 0; i--)
 	{if (in1_j[i] == in3_j[i]);
 	 else if (in1_j[i] > in3_j[i])
 	 {daeso = 0; goto dae;}
@@ -159,6 +175,30 @@ void compare(){				// 빼기, 나누기 전 크기 비교하는 함수
 	 else {daeso = 1; break;}}}
 dae:;
 } // compare 함수
+
+void compare_div(){				// 나누기에 사용하는 크기 비교하는 함수
+
+	res_d = strlen(res_j);
+
+	if (temp3_d > res_d)
+	daeso = 1;
+	else if(temp3_d < res_d)
+	{daeso = 0; goto dae;}
+	else{
+	for (int i = 0; i <= 49; i++)
+	{if (res_j[i] == in3_j[i]);
+	 else if (res_j[i] > in3_j[i])
+	 {daeso = 0; goto dae;}
+	 else {daeso = 1; break;}}}
+
+	 if (daeso == 0) {
+	 for (int i = 0; i <= 8; i++) {
+	 if (res_s[i] == in3_s[i]);
+	 else if (res_s[i] > in3_s[i])
+		  {daeso = 0; break;}
+	 else {daeso = 1; break;}}}
+dae:;
+} // compare_div 함수
 
 void convert1(){		// char > int 변환 함수
 
@@ -184,7 +224,7 @@ void convert1(){		// char > int 변환 함수
 }  // convert1 함수 
 
 
-void convert2(){		// int > char 변환 함수
+void convert2(){		// int > char 변환 함수 - 마이너스 일때
 
 	for (int i = 49; i >= 0; i--) {
 	if  (res_j[i] != 0)
@@ -209,6 +249,42 @@ void convert2(){		// int > char 변환 함수
 	p = strlen(res_s);
 
 }  // convert2 함수 
+/*
+void convert3(){		// int > char 변환 함수 - 플러스 일때
+
+	for (int i = 49; i >= 0; i--)			{		// 수를 문자로 바꾸기
+	if (res_j[i] != 0)
+	{temp_j = i; break;}					}
+	
+	for (int i = 0; i <= temp_j; i++)
+	res_j[i] += '0';
+	
+	for (int i = 8; i >= 0; i--)			{		// 수를 문자로 바꾸기
+	if (res_s[i] != 0)
+	{temp_s = i; break;}					}
+
+	for (int i = 0; i <= temp_s; i++)
+	res_s[i] += '0';
+
+} // convert3 함수
+*/
+void plus(){
+
+	for (int i = 8; i >= 0; i--)			{	// 소수 더하기
+	res_s[i] = in1_s[i] + in3_s[i] + k;
+	if (res_s[i] >= 10) 	{
+		k = 1; res_s[i] -= 10;}					// 오류 생길 수 있다.
+	else k = 0;								}
+	
+	p = strlen(res_s);
+	
+	for (int i = 0; i <= 49; i++)			{		// 정수 더하기
+	res_j[i] = in1_j[i] + in3_j[i] + k;
+	if (res_j[i] >= 10)	{
+		k = 1; res_j[i] -= 10;}
+	else k = 0;								}
+
+} // plus 함수 
 
 void minus(){
 
@@ -251,34 +327,84 @@ void minus(){
 	else k = 0;								}
 
 	}
+} // minus 함수
+
+void minus_div(){   // 나누기용 마이너스 함수
+
+	for (int i = 8; i >= 0; i--)			{	// 소수 빼기
+	res_s[i] = res_s[i] - in3_s[i] - k;
+	if ( i != 0)	{
+	if (res_s[i] < 0) 	{
+		k = 1; res_s[i] += 10;}					// 오류 생길 수 있다.
+	else k = 0;								}}
+	if (res_s[0] < 0)
+		{res_s[0] += 10; k = 1;}
+	else k = 0;
+	
+	for (int i = 0; i <= 49; i++)			{		// 정수 빼기
+	res_j[i] = res_j[i] - in3_j[i] - k;
+	if (res_j[i] < 0)	{
+		k = 1; res_j[i] += 10;}
+	else k = 0;								}
+
+} // minus_div 함수
+
+void divide(){
+
+	if ( daeso == 1 )
+	{res_j[0] = 0; daeso = 0; return ;}
+
+	else {
+	minus(); compare_div(); div_j[0] = 1;
+	if (daeso == 1) ;
+	else {
+	while( daeso != 1 ) {
+	minus_div();
+	compare_div();
+	div_j[0]++;
+	for(int i = 0; i <= 49; i++){
+	while (div_j[i] >= 10)
+	{div_j[i] -= 10; div_j[i+1]++;}}
+	}}}
+
+	for (int i = 0; i <= 49; i++)
+		res_j[i] = div_j[i];
+	for (int i = 0; i <= 8; i++)
+		res_s[i] = 0;
+	daeso = 0;
+} // divide 함수
+
+
+
+void initialization(){
+
+for (int i = 0; i <= 59; i++)
+{
+in1[i] = 0;
+in2[i] = 0;
+in3[i] = 0;
+res[i] = 0;
+}
+for (int i = 0; i <= 49; i++)
+{
+in1_j[i] = 0;
+in3_j[i] = 0;
+res_j[i] = 0;
+div_j[i] = 0;
+}
+for (int i = 0; i <= 8; i++)
+{
+in1_s[i] = 0;
+in3_s[i] = 0;
+res_s[i] = 0;
 }
 
+str_in1 = 0, str_in2 = 0, str_in3 = 0, str_res_s = 0;
+temp = 0, temp1_d = 0, temp3_d = 0, temp_j = 0, temp_s = 0;
+first = 0, last = 0;
+k = 0, p = 0, q = 0, daeso = 0, choice = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+} // 초기화 함수
 
 
 
